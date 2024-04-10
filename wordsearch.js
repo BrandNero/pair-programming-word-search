a49e05afaad3df2e194a199aaa9894f0a99a505f
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-10 17:33:05 -0400

    added for vertical check

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  const transposedLetters = transpose(letters);
+  console.log(transposedLetters);
+  const verticalJoin = transposedLetters.map(ls => ls.join(''));
+  for (let v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+  
+  return false;
+};
+  
+const transpose = function(letter) {
+  const transposeMatrix = [];
+  for (let col = 0; col < letter[0].length; col++) {
+    transposeMatrix[col] = [];
+    for (let row = 0; row < letter.length; row ++) {
+      transposeMatrix[col][row] = letter[row][col];
     }
-}
-
-module.exports = wordSearch
+  }
+  return transposeMatrix;
+};
+module.exports = wordSearch;
